ec1dc2575c2d5e6cc32cd536d1f994d5f5d9f450
DavidLee999 <MoonLee003@outlook.com>
2017-05-25 10:16:59 +0200

    first

---
+++ Ch1/hellojava.java
@@ -0,0 +1,24 @@
+package first;
+
+import java.util.Scanner;
+
+import java.io.*;
+
+
+public class hellojava {
+	
+	public static void main(String []args) {
+		Scanner scan = new Scanner(System.in);
+		
+		if ( scan.hasNextLine())
+		{
+			String str = scan.nextLine();
+			System.out.println(str);
+		}
+		
+		File f = new File;
+		FileInputStream fip = new FileInputStream(f);
+		
+	}
+	
+}
