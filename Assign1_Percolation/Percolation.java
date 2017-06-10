b2c568e27ed5d053365b08405a68b86df1bbf0fc
DavidLee999 <MoonLee003@outlook.com>
2017-06-10 12:08:32 +0200

    Percolations

--- Assign1_Percolation/Percolation.java
+++ Assign1_Percolation/Percolation.java
@@ -12,6 +12,7 @@ public class Percolation {
 	{
 		if ( N <= 0)
 			throw new IllegalArgumentException("The size must be bigger than 0.");
+		
 		size = N;
 		
 		bottom = size * size + 1;
@@ -20,37 +21,70 @@ public class Percolation {
 		
 		wqu = new WeightedQuickUnionUF(size * size + 2);
 	}
-	/*
-	private void validate( int p )
+	private boolean validate( int p )
 	{
-		
+		if ( p < 1 || p > size)
+			return false;
+			//throw new IndexOutOfBoundsException("index " + p + " is not between 1 and " + size);
+		else
+			return true;
 	}
 	
-	private int xy2id(int x, int y)
+	private int xy2id(int row, int col)
 	{
+		int id = size * (row - 1) + col;
 		
-		return 0;
+		return id;
 	}
 	
-	public void open(int x, int y)
+	public void open(int row, int col)
 	{
+		if ( !validate(row) || !validate(col) )
+			throw new IndexOutOfBoundsException("the index is not between 1 and " + size);
 		
+		int id = xy2id(row, col);
+		
+		grid[row-1][col-1] = true;
+		
+		if ( row == 1 )
+			wqu.union(id, top);
+		
+		if ( row == size )
+			wqu.union(id, top);
+		
+		if ( validate(row-1) && isOpen(row-1, col) )
+			wqu.union( id, xy2id(row-1, col) );
+		
+		if ( validate(row+1) && isOpen(row+1, col) )
+			wqu.union( id, xy2id(row+1, col) );
+		
+		if ( validate(col-1) && isOpen(row, col-1) )
+			wqu.union( id, xy2id(row, col-1) );
+		
+		if ( validate(col+1) && isOpen(row, col+1) )
+			wqu.union( id, xy2id(row, col+1) );
 	}
 	
-	public boolean isOpen(int x, int y)
+	public boolean isOpen(int row, int col)
 	{
+		if ( !validate(row) || !validate(col) )
+			throw new IndexOutOfBoundsException("the index is not between 1 and " + size);
 		
+		return grid[row-1][col-1];
 	}
 	
-	public boolean  isFull(int x, int y)
+	public boolean  isFull(int row, int col)
 	{
+		if ( !validate(row) || !validate(col) )
+			throw new IndexOutOfBoundsException("the index is not between 1 and " + size);
 		
+		return wqu.connected( top, xy2id(row, col) );
 	}
 	
 	public boolean isPercolated()
 	{
-		
-	}*/
+		return wqu.connected(top, bottom);
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
