60e651d33bcf3a97e740b92dda3d19f16c6404b3
DavidLee999 <MoonLee003@outlook.com>
2017-07-09 13:44:25 +0200

    update

--- Ch2/LinkedListStack.java
+++ Ch2/LinkedListStack.java
@@ -2,12 +2,12 @@ import edu.princeton.cs.algs4.StdIn;
 import java.util.Iterator;
 
 public class LinkedListStack<Item> implements Iterable<Item> {
-	
-	private class Node<Item> {
-		private Item item;
-		private Node<Item> next;
-	}
-	
+
+    private class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+
     private class ListIterator<Item> implements Iterator<Item> {
         private Node<Item> current;
 
@@ -34,44 +34,44 @@ public class LinkedListStack<Item> implements Iterable<Item> {
             throw new UnsupportedOperationException();
         }
     }
-	private Node<Item> first;
+    private Node<Item> first;
     private int n;
 
     public LinkedListStack(){
         first = null;
         n = 0;
     }
-	
-	public boolean isEmpty()
-	{ return first == null; }
+
+    public boolean isEmpty()
+    { return first == null; }
 
     public int size() { return n; }
-	
-	public void push( Item item )
-	{
-		Node<Item> oldfirst = first;
-		
-		first = new Node<Item>();
-		
-		first.item = item;
-		first.next = oldfirst;
+
+    public void push( Item item )
+    {
+        Node<Item> oldfirst = first;
+
+        first = new Node<Item>();
+
+        first.item = item;
+        first.next = oldfirst;
 
         n++;
-	}
-	
-	public Item pop()
-	{
+    }
+
+    public Item pop()
+    {
         if( isEmpty() )
             throw new IndexOutOfBoundsException("The Stack is Empty!");
 
-		Item item = first.item;
-		
-		first = first.next;
-		
+        Item item = first.item;
+
+        first = first.next;
+
         n--;
 
-		return item;
-	}
+        return item;
+    }
 
     public Item peak()
     {
@@ -85,28 +85,28 @@ public class LinkedListStack<Item> implements Iterable<Item> {
     {
         return new ListIterator<Item>( first );
     }
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LinkedListStack<String> st = new LinkedListStack<String>();
-		
-		while( !StdIn.isEmpty() ){
-			String s = StdIn.readString();
-			
-			if ( s.equals( "-" ) ) {
-				if ( st.isEmpty() )
-					System.out.println( "Empty!" );
-				else
-					System.out.println( st.pop() );
-			}
-			else
-				st.push( s );
-		}
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        LinkedListStack<String> st = new LinkedListStack<String>();
+
+        while( !StdIn.isEmpty() ){
+            String s = StdIn.readString();
+
+            if ( s.equals( "-" ) ) {
+                if ( st.isEmpty() )
+                    System.out.println( "Empty!" );
+                else
+                    System.out.println( st.pop() );
+            }
+            else
+                st.push( s );
+        }
 
         for( String i : st )
             System.out.print( i + " " );
 
         System.out.print( st.size() );
-	}
+    }
 
 }
