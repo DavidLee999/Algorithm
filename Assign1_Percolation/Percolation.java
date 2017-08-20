a3c23e5e4de922e907d3beed65638d8c107dc02a
DavidLee999 <MoonLee003@outlook.com>
2017-08-20 16:35:00 +0200

    Percolation

--- Assign1_Percolation/Percolation.java
+++ Assign1_Percolation/Percolation.java
@@ -2,117 +2,117 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
-	private boolean[][] grid;
-	private int size;
-	private int top;
-	private int bottom;
-	private int counter;
-	private WeightedQuickUnionUF wqu;
-	private WeightedQuickUnionUF no_bottom_qu;
-	
-	public Percolation(int N)
-	{
-		if (N <= 0)
-			throw new IllegalArgumentException("The size must be bigger than 0.");
-		
-		size = N;
-		
-		top = 0;
-		
-		bottom = size * size + 1;
-		
-		grid = new boolean[size][size];
-		
-		counter = 0;
-		
-		wqu = new WeightedQuickUnionUF(size * size + 2);
-		no_bottom_qu = new WeightedQuickUnionUF(size * size + 1);
-	}
-	
-	private boolean validate(int p)
-	{
-		if (p < 1 || p > size)
-			return false;
-			// throw new IndexOutOfBoundsException("index " + p + " is not between 1 and " + size);
-		else
-			return true;
-	}
-	
-	private int xy2id(int row, int col)
-	{
-		int id = size * (row - 1) + col;
-		
-		return id;
-	}
-	
-	public void open(int row, int col)
-	{
-		if (!validate(row) || !validate(col))
-			throw new IndexOutOfBoundsException("the index is not between 1 and " + size);
-		
-		if (isOpen(row, col))
-			return;
-		
-		int id = xy2id(row, col);
-		
-		grid[row-1][col-1] = true;
-		
-		counter += 1;
-		
-		if (row == 1){
-			wqu.union(id, top);
-			no_bottom_qu.union(id, top);
-		}
-		
-		if (row == size)
-			wqu.union(id, bottom);
-		
-		if (validate(row-1) && isOpen(row-1, col)){
-			wqu.union(id, xy2id(row-1, col));
-			no_bottom_qu.union(id, xy2id(row-1, col));
-		}
-		
-		if (validate(row+1) && isOpen(row+1, col)){
-			wqu.union(id, xy2id(row+1, col));
-			no_bottom_qu.union( id, xy2id(row+1, col));
-		}
-		
-		if (validate(col-1) && isOpen(row, col-1)){
-			wqu.union( id, xy2id(row, col-1) );
-			no_bottom_qu.union( id, xy2id(row, col-1));
-		}
-		
-		if (validate(col+1) && isOpen(row, col+1)){
-			wqu.union(id, xy2id(row, col+1) );
-			no_bottom_qu.union(id, xy2id(row, col+1));
-		}
-	}
-	
-	public boolean isOpen(int row, int col)
-	{
-		if (!validate(row) ||!validate(col))
-			throw new IndexOutOfBoundsException("the index is not between 1 and " + size);
-		
-		return grid[row-1][col-1];
-	}
-	
-	public boolean  isFull(int row, int col)
-	{
-		if (!validate(row) || !validate(col))
-			throw new IndexOutOfBoundsException("the index is not between 1 and " + size);
-		
-		return no_bottom_qu.connected(top, xy2id(row, col));
-	}
-	
-	public int numberOfOpenSites() { return counter; }
-	
-	public boolean percolates()
-	{
-		return wqu.connected(top, bottom);
-	}
-	
-	public static void main(String[] args) {
-		
-	}
+    private boolean[][] grid;
+    private int size;
+    private int top;
+    private int bottom;
+    private int counter;
+    private WeightedQuickUnionUF wqu;
+    private WeightedQuickUnionUF no_bottom_qu;
+
+    public Percolation(int N)
+    {
+        if (N <= 0)
+            throw new IllegalArgumentException("The size must be bigger than 0.");
+
+        size = N;
+
+        top = 0;
+
+        bottom = size * size + 1;
+
+        grid = new boolean[size][size];
+
+        counter = 0;
+
+        wqu = new WeightedQuickUnionUF(size * size + 2);
+        no_bottom_qu = new WeightedQuickUnionUF(size * size + 1);
+    }
+
+    private boolean validate(int p)
+    {
+        if (p < 1 || p > size)
+            return false;
+        // throw new IndexOutOfBoundsException("index " + p + " is not between 1 and " + size);
+        else
+            return true;
+    }
+
+    private int xy2id(int row, int col)
+    {
+        int id = size * (row - 1) + col;
+
+        return id;
+    }
+
+    public void open(int row, int col)
+    {
+        if (!validate(row) || !validate(col))
+            throw new IndexOutOfBoundsException("the index is not between 1 and " + size);
+
+        if (isOpen(row, col))
+            return;
+
+        int id = xy2id(row, col);
+
+        grid[row-1][col-1] = true;
+
+        counter += 1;
+
+        if (row == 1){
+            wqu.union(id, top);
+            no_bottom_qu.union(id, top);
+        }
+
+        if (row == size)
+            wqu.union(id, bottom);
+
+        if (validate(row-1) && isOpen(row-1, col)){
+            wqu.union(id, xy2id(row-1, col));
+            no_bottom_qu.union(id, xy2id(row-1, col));
+        }
+
+        if (validate(row+1) && isOpen(row+1, col)){
+            wqu.union(id, xy2id(row+1, col));
+            no_bottom_qu.union( id, xy2id(row+1, col));
+        }
+
+        if (validate(col-1) && isOpen(row, col-1)){
+            wqu.union( id, xy2id(row, col-1) );
+            no_bottom_qu.union( id, xy2id(row, col-1));
+        }
+
+        if (validate(col+1) && isOpen(row, col+1)){
+            wqu.union(id, xy2id(row, col+1) );
+            no_bottom_qu.union(id, xy2id(row, col+1));
+        }
+    }
+
+    public boolean isOpen(int row, int col)
+    {
+        if (!validate(row) ||!validate(col))
+            throw new IndexOutOfBoundsException("the index is not between 1 and " + size);
+
+        return grid[row-1][col-1];
+    }
+
+    public boolean  isFull(int row, int col)
+    {
+        if (!validate(row) || !validate(col))
+            throw new IndexOutOfBoundsException("the index is not between 1 and " + size);
+
+        return no_bottom_qu.connected(top, xy2id(row, col));
+    }
+
+    public int numberOfOpenSites() { return counter; }
+
+    public boolean percolates()
+    {
+        return wqu.connected(top, bottom);
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
--- Assign1_Percolation/PercolationStats.java
+++ Assign1_Percolation/PercolationStats.java
@@ -3,88 +3,88 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdOut;
 
 public class PercolationStats {
-	// private Percolation pr;
-	private double[] f;
-	private int size;
-	// private int num;
-	
-	public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
-	{
-		if (n <= 0)
-			throw new IllegalArgumentException("The size must be bigger than 0.");
-		if (trials <= 0)
-			throw new IllegalArgumentException("The experiment times must be bigger than 9.");
-		
-		size = n;
-		
-		// num = trials;
-		
-		// pr = new Percolation( size );
-		
-		f = new double[trials];
-		
-		experiments();
-	}
-	
-	private int randPercolates()
-	{
-		// int conter = 0;
-		Percolation pr = new Percolation(size);
-		while (!pr.percolates())
-		{
-			int p = StdRandom.uniform(1, size + 1);
-			int q = StdRandom.uniform(1, size + 1);
-			
-			if (!pr.isOpen(p, q))
-				pr.open( p, q );
-		}
-		
-		return pr.numberOfOpenSites();
-	}
-	
-	private void experiments()
-	{
-		int counter = 0;
-		while (counter < f.length)
-		{
-			int numOfOpenSites = randPercolates();
-			f[counter] = (double)(numOfOpenSites) / (size * size) ;
-			
-			counter += 1;
-		}
-	}
-	
-	public double mean()                          // sample mean of percolation threshold
-	{
-		return StdStats.mean(f);
-	}
-	
-	public double stddev()                        // sample standard deviation of percolation threshold
-	{
-		return StdStats.stddev(f);
-	}
-	
-	public double confidenceLo()                  // low  endpoint of 95% confidence interval
-	{
-		return mean() - 1.96 * stddev() / Math.sqrt(f.length);
-	}
-	
-	public double confidenceHi()                  // high endpoint of 95% confidence interval
-	{
-		return mean() + 1.96 * stddev() / Math.sqrt(f.length);
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		int n = Integer.parseInt(args[0]);
-		int trials = Integer.parseInt(args[1]);
-		
-		PercolationStats PS = new PercolationStats(n, trials);
-		
-		StdOut.printf("Mean                    = %1.16f%n", PS.mean());
-		StdOut.printf("Stddev                  = %1.16f%n", PS.stddev());
-		StdOut.printf("95%% confidence interval = [%1.16f %1.16f]%n", PS.confidenceLo(), PS.confidenceHi());
-	}
+    // private Percolation pr;
+    private double[] f;
+    private int size;
+    // private int num;
+
+    public PercolationStats(int n, int trials)    // perform trials independent experiments on an n-by-n grid
+    {
+        if (n <= 0)
+            throw new IllegalArgumentException("The size must be bigger than 0.");
+        if (trials <= 0)
+            throw new IllegalArgumentException("The experiment times must be bigger than 9.");
+
+        size = n;
+
+        // num = trials;
+
+        // pr = new Percolation( size );
+
+        f = new double[trials];
+
+        experiments();
+    }
+
+    private int randPercolates()
+    {
+        // int conter = 0;
+        Percolation pr = new Percolation(size);
+        while (!pr.percolates())
+        {
+            int p = StdRandom.uniform(1, size + 1);
+            int q = StdRandom.uniform(1, size + 1);
+
+            if (!pr.isOpen(p, q))
+                pr.open( p, q );
+        }
+
+        return pr.numberOfOpenSites();
+    }
+
+    private void experiments()
+    {
+        int counter = 0;
+        while (counter < f.length)
+        {
+            int numOfOpenSites = randPercolates();
+            f[counter] = (double)(numOfOpenSites) / (size * size) ;
+
+            counter += 1;
+        }
+    }
+
+    public double mean()                          // sample mean of percolation threshold
+    {
+        return StdStats.mean(f);
+    }
+
+    public double stddev()                        // sample standard deviation of percolation threshold
+    {
+        return StdStats.stddev(f);
+    }
+
+    public double confidenceLo()                  // low  endpoint of 95% confidence interval
+    {
+        return mean() - 1.96 * stddev() / Math.sqrt(f.length);
+    }
+
+    public double confidenceHi()                  // high endpoint of 95% confidence interval
+    {
+        return mean() + 1.96 * stddev() / Math.sqrt(f.length);
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+
+        PercolationStats PS = new PercolationStats(n, trials);
+
+        StdOut.printf("Mean                    = %1.16f%n", PS.mean());
+        StdOut.printf("Stddev                  = %1.16f%n", PS.stddev());
+        StdOut.printf("95%% confidence interval = [%1.16f %1.16f]%n", PS.confidenceLo(), PS.confidenceHi());
+    }
 
 }
