e82e82e689ce63995510475a6ad9c4344703ef1f
DavidLee999 <MoonLee003@outlook.com>
2017-09-06 20:30:15 +0200

    update

--- Ch1/Quick_Find.java
+++ Ch1/Quick_Find.java
@@ -3,74 +3,74 @@ import edu.princeton.cs.algs4.StdOut;
 
 public class Quick_Find {
 
-	private int[] id;
-	private int count;
-	
-	public Quick_Find(int N)
-	{
-		id = new int[N];
-		count = N;
-		for (int i = 0; i < N; i++)
-			id[i] = i;
-	}
+    private int[] id;
+    private int count;
 
-	private void validate(int p) 
-	{
+    public Quick_Find(int N)
+    {
+        id = new int[N];
+        count = N;
+        for (int i = 0; i < N; i++)
+            id[i] = i;
+    }
+
+    private void validate(int p) 
+    {
         int n = id.length;
-        
+
         if (p < 0 || p >= n) 
         {
             throw new IndexOutOfBoundsException("index " + p + " is not between 0 and " + (n-1));
         }
     }
-	
-	public boolean connected(int p, int q)
-	{
-		validate(p);
-		validate(q);
-		
-		return id[p] == id[q];
-	}
-	
-	public void union(int p, int q)
-	{
-		validate(p);
-		validate(q);
-		
-		if ( id[p] == id[q] ) return;
-		
-		int pid = id[p];
-		int qid = id[q];
-		
-		for (int i = 0; i < id.length; i++)
-		{
-			if (id[i] == pid)
-				id[i] = qid;
-		}
-		--count;
-	}
-	
-	public int count()
-	{
-		return count;
-	}
-	
-	public static void main(String[] args)
-	{
-		  int n = StdIn.readInt();
-		  Quick_Find qf = new Quick_Find(n);
-		  
-		  while (!StdIn.isEmpty())
-		  {
-			  int p = StdIn.readInt();
-			  int q = StdIn.readInt();
-			  
-			  if(qf.connected(p, q))
-				  continue;
-			  qf.union(p, q);
-			  
-			  StdOut.println(p + " " + q);
-		  }
-		  StdOut.println(qf.count() + " components.");
-	}
+
+    public boolean connected(int p, int q)
+    {
+        validate(p);
+        validate(q);
+
+        return id[p] == id[q];
+    }
+
+    public void union(int p, int q)
+    {
+        validate(p);
+        validate(q);
+
+        if ( id[p] == id[q] ) return;
+
+        int pid = id[p];
+        int qid = id[q];
+
+        for (int i = 0; i < id.length; i++)
+        {
+            if (id[i] == pid)
+                id[i] = qid;
+        }
+        --count;
+    }
+
+    public int count()
+    {
+        return count;
+    }
+
+    public static void main(String[] args)
+    {
+        int n = StdIn.readInt();
+        Quick_Find qf = new Quick_Find(n);
+
+        while (!StdIn.isEmpty())
+        {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+
+            if(qf.connected(p, q))
+                continue;
+            qf.union(p, q);
+
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(qf.count() + " components.");
+    }
 }
